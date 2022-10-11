63089f2e4315872a78518627de9e79a18780ecbd
Adrian Vasiliu <adrian.vasiliu@ontegra.com>
2022-10-11 19:44:11 +0300

    2 din 3 buline momentan :)

---
+++ Controllers/SecondController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vasiliu_Adrian_Lab1.Controllers
+{
+    public class SecondController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Hello()
+        {
+            return View();
+        }
+
+        public IActionResult Show(string str, int n)
+        {
+            return View();
+        }
+
+    }
+}
